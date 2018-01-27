e638e73d3127a3fccb82e498fd8afe014806037d
Kraiphit <kraiphit.k@ku.th>
2018-01-27 14:40:33 +0700

    add my initial code

---
+++ src/test/java/bank/BankAccountTest.java
@@ -0,0 +1,14 @@
+package bank;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class BankAccountTest {
+
+    BankAccount bak
+    @BeforeEach
+    public void setup(){
+        bak = new BankAccount();
+        System.out.println("Hello");
+
+
+}
